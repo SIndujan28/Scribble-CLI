5f0b572955ebe7ca08fae998c50e4038438d27f7
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-02-19 23:07:50 +0530

    add session login/logout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React,{ useState } from "react";
+import React,{ useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 
 import Routes from "./Routes";
 import "./App.css";
 
 function App(props) {
   const [isAuthenticated, userHasAuthenticated] =useState(false);
+  const [isAuthenticating, setIsAuthenticating] =useState(true);
   console.log(typeof(userHasAuthenticated))
-  function handleLogout() {
+  async function handleLogout() {
+    await Auth.signOut();
     userHasAuthenticated(false);
-    console.log(isAuthenticated)
     }
+  useEffect(() => {
+    onLoad();
+    }, []);
+
+  async function onLoad() {
+      try {
+        await Auth.currentSession();
+        userHasAuthenticated(true);
+      }
+      catch(e) {
+        if (e !== 'No current user') {
+        alert(e);
+          }
+        }
+      setIsAuthenticating(false);
+      }
+
+
   return (
+  !isAuthenticating &&
   <div className="App container">
      <Navbar fluid collapseOnSelect>
        <Navbar.Header>
